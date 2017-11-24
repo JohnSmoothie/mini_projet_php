7f64b438dd045fa334be720f6003e1a476936f5a
E164786Q <evann.bacala@etu.univ-nantes.fr>
2017-11-24 13:57:01 +0100

    test evann 2

--- ressources/crypt.php
+++ ressources/crypt.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 
 $toto=crypt('toto');
 echo $toto."<br/>";
@@ -7,7 +6,7 @@ echo $toto."<br/>";
 $titi=crypt('titi');
 echo $titi."<br/>";
 
-// il faut que la fonction crypt() connaisse la méthode de cryptage et le "sel" à utiliser. 
+// il faut que la fonction crypt() connaisse la méthode de cryptage et le "sel" à utiliser.
 //Il faut que ce soit les mêmes que ce qui a été utilisé lors du cryptage.
 // ces 2 informations sont stockées au début de la chaîne de caractères résultant du cryptage
 //C'est pour cette raison que l'on passe $toto comme 2ème paramètre.
