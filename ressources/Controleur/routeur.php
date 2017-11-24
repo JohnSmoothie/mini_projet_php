73f0ba86182897662491fc7e28634953d008849f
ebacala <e.bacala@gmail.com>
2017-11-24 15:47:10 +0100

    kj

--- ressources/Controleur/routeur.php
+++ ressources/Controleur/routeur.php
@@ -14,7 +14,7 @@ class Routeur
     // Traite une requête entrante
     public function routerRequete()
     {
-        if (isset($_POST['login']) && isset($_POST['pwd'])) {
+        if (isset($_POST["login"]) && isset($_POST["pwd"])) {
             $this->ctrlAuthentification->authentification($_POST['login'], $_POST['pwd']);
         } else {
             $this->ctrlAuthentification->accueil();
--- ressources/index.php
+++ ressources/index.php
@@ -3,10 +3,6 @@
 require "config/config.php";
 require PATH_CONTROLEUR."/routeur.php";
 
-// if(isset($_POST['login'])) {
-//   setCookie("cookiePseudo", $_POST['login']);
-// }
-
 $routeur=new Routeur();
 $routeur->routerRequete();
 
