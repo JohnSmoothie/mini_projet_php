978908aa67b47893181933264561b50f1f02e552
johnSmoothie <vicoboix@gmail.com>
2017-11-24 16:16:23 +0100

    création de la vue jeux

--- ressources/index.php
+++ ressources/index.php
@@ -1,13 +1,16 @@
 <?php
 
 require "config/config.php";
+require PATH_VUE."/Jeux.php";
 require PATH_CONTROLEUR."/routeur.php";
 
 if(isset($_POST['login'])) {
   setCookie("cookiePseudo", $_POST['login']);
 }
 
-$routeur=new Routeur();
-$routeur->routerRequete();
+//$routeur=new Routeur();
+$jeux=new Jeux();
+$jeux->afficherPage();
+//$routeur->routerRequete();
 
 ?>
