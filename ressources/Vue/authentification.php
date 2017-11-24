b4bc5afb4e525e302f1cc8a274f4eacdc5466f0a
E164786Q <evann.bacala@etu.univ-nantes.fr>
2017-11-24 14:24:39 +0100

    Modification de authentification.php

--- ressources/Vue/authentification.php
+++ ressources/Vue/authentification.php
@@ -1,14 +1,26 @@
-<html>
-  <head>
-    <meta charset="utf-8" />
-  </head>
-  <body>
-      <form method="post" action="index.php">
-        Login
-        <input type="text" name="login"/><br /><br />
-        Mot de passe
-        <input type="password" name="pwd"/><br /><br />
-        <input type="submit" name="soumettre" value="envoyer"/>
-    </form>
-  </body>
-</html>
+<<?php
+  class Authentification {
+
+    function afficherPage() {
+      ?>
+
+      <html>
+        <head>
+          <meta charset="utf-8" />
+        </head>
+        <body>
+            <form method="post" action="index.php">
+              Login
+              <input type="text" name="login"/><br /><br />
+              Mot de passe
+              <input type="password" name="pwd"/><br /><br />
+              <input type="submit" name="soumettre" value="envoyer"/>
+          </form>
+        </body>
+      </html>
+
+      <?php
+    }
+
+  }
+  ?>
