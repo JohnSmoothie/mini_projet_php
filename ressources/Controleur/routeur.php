ad5100cc4faf314e6f4f52ce8c5ef76dbb89dc76
E164786Q <evann.bacala@etu.univ-nantes.fr>
2017-12-01 14:15:22 +0100

    .

--- ressources/Controleur/routeur.php
+++ ressources/Controleur/routeur.php
@@ -1,28 +1,22 @@
 <?php
 
-require_once 'Controleur_authentification.php';
+  require_once 'Controleur_authentification.php';
 
-class Routeur {
+  class Routeur {
 
-  private $controleur_authentification;
+    private $controleur_authentification;
 
-  public function __construct() {
-    $this->controleur_authentification = new Controleur_authentification();
+    public function __construct() {
+      $this->controleur_authentification = new Controleur_authentification();
+    }
+
+    public function routerRequete() {
+      if (isset($_POST['login']) && isset($_POST['password'])) {
+        $this->controleur_authentification->verifieConnexion($_POST['login'], $_POST['password']);
+      }
+      else {
+        $this->controleur_authentification->accueil();
+      }
+    }
   }
-
-  // Traite une requête entrante
-  public function routerRequete() {
-
-    if (isset($_POST['login']) && isset($_POST['password'])) {
-      $this->controleur_authentification->verifieConnexion($_POST['login'], $_POST['password']);
-     }
-    else {$this->controleur_authentification->accueil();}
- }
-
-
- }
-
-
-
-
 ?>
