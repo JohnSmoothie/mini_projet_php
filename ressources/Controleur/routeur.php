fe8a2d1a1d165ee428c0d76dfdefef40dd96f99c
ebacala <e.bacala@gmail.com>
2017-12-05 19:58:15 +0100

    Ajout de boutton de déconnexion

--- ressources/Controleur/routeur.php
+++ ressources/Controleur/routeur.php
@@ -1,26 +1,33 @@
 <?php
 
+require_once 'ControleurAuthentification.php';
+require_once 'ControleurPartie.php';
 
-  require_once 'Controleur_authentification.php';
-  require_once 'Controleur_partie.php';
+class Routeur
+{
+    private $controleur_authentification;
+    private $controleur_partie;
 
-  class Routeur {
-
-    private $Controleur_authentification;
-    private $Controleur_partie;
-
-    public function __construct() {
-      $this->controleur_authentification = new Controleur_authentification();
-      $this->controleur_partie = new Controleur_partie();
+    public function __construct()
+    {
+        $this->controleur_authentification = new ControleurAuthentification();
+        $this->controleur_partie = new ControleurPartie();
     }
 
-    public function routerRequete() {
-      if (isset($_SESSION["login"]) && isset($_SESSION['password'])) {
-        $this->controleur_authentification->verifieConnexion($_POST['login'], $_POST['password']);
-      }
-      else {
-        $this->controleur_authentification->accueil();
-      }
+    public function routerRequete()
+    {
+        if (isset($_POST['login']) && isset($_POST['password'])) {
+            if ($this->controleur_authentification->verifieConnexion($_POST['login'], $_POST['password'])) {
+                $_SESSION['login'] = $_POST['login'];
+                $_SESSION['password'] = $_POST['password'];
+                $this->controleur_partie->jeu();
+            } else {
+                $this->controleur_authentification->afficherVueAuthentification();
+            }
+        } else {
+            $this->controleur_authentification->afficherVueAuthentification();
+        }
     }
-  }
+}
+
 ?>
