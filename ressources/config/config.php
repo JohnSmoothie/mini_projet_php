bb37797803a5b0c41929f2122abd48fc8796d3f4
ebacala <e.bacala@gmail.com>
2017-12-06 15:00:35 +0100

    Ajout de bouton de déconnexion

--- ressources/config/config.php
+++ ressources/config/config.php
@@ -1,11 +1,22 @@
 <?php
 
-  define("HOME_SITE", __DIR__ . "/..");
-  define("PATH_VUE", HOME_SITE . "/Vue");
-  define("PATH_CONTROLEUR", HOME_SITE . "/Controleur");
-  define("PATH_MODELE", HOME_SITE . "/Modele");
-  define("HOST", "localhost");
-  define("BD", "E164786Q");
-  define("LOGIN", "E164786Q");
-  define("PASSWORD", "E164786Q");
+define("HOME_SITE", __DIR__ . "/..");
+define("PATH_VUE", HOME_SITE . "/Vue");
+define("PATH_CONTROLEUR", HOME_SITE . "/Controleur");
+define("PATH_MODELE", HOME_SITE . "/Modele");
+
+// BASE IUT
+/*
+define("HOST", "localhost");
+define("BD", "E164786Q");
+define("LOGIN", "E164786Q");
+define("PASSWORD", "E164786Q");
+*/
+
+// BASE EVANN
+
+define("HOST", "192.168.0.103");
+define("BD", "mini_projet_php");
+define("LOGIN", "ebacala");
+define("PASSWORD", "ebacala");
 ?>
